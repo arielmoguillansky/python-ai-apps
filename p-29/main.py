e1ac7c6c723f4c2ecea50d2730ba63730873e548
arielmoguillansky <arielmogui92@gmail.com>
2026-05-22 13:31:02 -0300

    [p-29] Password manager

--- p-29/main.py
+++ p-29/main.py
@@ -2,6 +2,22 @@ from tkinter import *
 from tkinter import messagebox
 from password_generator import generate_password
 import pyperclip
+import json
+
+def search():
+    website = website_entry.get().lower()
+    try:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showinfo(title=website, message="No entry found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} \n Password: {password}")
+        else:
+            messagebox.showinfo(title=website, message="No entry found.")
 
 def insert_generated_password():
     password_entry.delete(0, END)
@@ -14,6 +30,11 @@ def check_entries(*args):
     email = email_entry.get()
     password = password_entry.get()
 
+    if len(website) > 0:
+        search_btn.config(state="normal")
+    else:
+        search_btn.config(state="disabled")
+
     if len(website) > 0 and len(email) > 0 and len(password) > 0:
         add_btn.config(state="normal")
     else:
@@ -21,21 +42,29 @@ def check_entries(*args):
 
 
 def save():
-    website = website_entry.get()
-    email = email_entry.get()
+    website = website_entry.get().lower()
+    email = email_entry.get().lower()
     password = password_entry.get()
-
+    new_data = {website:{"email": email, "password": password}}
     confirm_save = messagebox.askokcancel(title=website,
                                           message=f"Your password has been saved: \n Email: {email} | Password:{password} \n Is this correct?")
 
     if confirm_save:
-        with open("data.txt", "a") as f:
-            f.write(f"{website} | {email} | {password}\n")
+        try:
+            with open("data.json", "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            with open("data.json", "w") as f:
+                json.dump(new_data, f, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as f:
+                json.dump(data, f, indent=4)
+        finally:
             website_entry.delete(0, END)
             email_entry.delete(0, END)
             password_entry.delete(0, END)
-
-        add_btn.config(state="disabled")
+            add_btn.config(state="disabled")
 
 
 window = Tk()
@@ -43,11 +72,9 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
-try:
-    logo_img = PhotoImage(file="logo.png")
-    canvas.create_image(100, 100, image=logo_img)
-except Exception:
-    pass
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+
 canvas.grid(row=0, column=1)
 
 # Labels
@@ -66,8 +93,9 @@ web_text.trace_add("write", check_entries)
 email_text.trace_add("write", check_entries)
 pass_text.trace_add("write", check_entries)
 
-website_entry = Entry(width=35, textvariable=web_text)
-website_entry.grid(row=1, column=1, columnspan=2)
+# Entries
+website_entry = Entry(width=21, textvariable=web_text)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
 email_entry = Entry(width=35, textvariable=email_text)
@@ -79,9 +107,9 @@ password_entry.grid(row=3, column=1)
 # Buttons
 generate_btn = Button(text="Generate Password", command=insert_generated_password)
 generate_btn.grid(row=3, column=2)
-
-# Starts disabled by default
 add_btn = Button(text="Add", width=36, command=save, state="disabled")
 add_btn.grid(row=4, column=1, columnspan=2)
+search_btn = Button(text="Search", width=15, command=search, state="disabled")
+search_btn.grid(row=1, column=2)
 
 window.mainloop()
